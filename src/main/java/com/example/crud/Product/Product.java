75ba69bb2cde70e04d6f60815afcad11b21717e3
andressamuel13 <samuelma236@gmail.com>
2025-09-01 20:48:30 -0500

    primer back creado en spring

---
+++ src/main/java/com/example/crud/Product/Product.java
@@ -0,0 +1,10 @@
+package com.example.crud.Product;
+
+import java.time.LocalDate;
+
+public class Product {
+    private Long id;
+    private String name;
+    private String price;
+    private LocalDate fecha;
+}
